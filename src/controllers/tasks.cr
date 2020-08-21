3dfbaefd55d7933db2ed951f3e4b9fdd70f744b8
Michał Połoński <polonski@gmail.com>
2020-08-21 10:44:25 +1000

    chore(tasks):cleanup, optimise and use clear savepoint transactions

--- src/controllers/tasks.cr
+++ src/controllers/tasks.cr
@@ -11,40 +11,25 @@ class Tasks < Application
     response.headers["Access-Control-Allow-Methods"] = "GET,HEAD,POST,DELETE,OPTIONS,PUT,PATCH"
   end
 
-  # before_action :find_task
-  # getter task : Task?
-
   def index
-    # all_tasks=nil
-    begin
-      all_tasks = Task.query.select.to_a
-      render text: all_tasks.to_json
-    rescue e
-      raise e
-    end
+    render text: Task.query.select.to_a.to_json
   end
 
   def show
-    t : Task = Task.query.find({id: params["id"]}).as(Task)
-
-    render text: t.to_json
+    render text: task(params["id"]).to_json
   end
 
   def create
-    # for some reason if this hash is not passed into a variable
-    # and directly placed inside Task.new() , it gived this err:
-    # Unexpected token: <EOF> at 1:1 (JSON::ParseException)
-    h = JSON.parse(request.body.as(IO))
-
-    t : Task? = Task.new(h)
-
+    t : Task? = Task.new(JSON.parse(request.body.as(IO))).as(Task)
     t.save
-    t.url = "https://#{request.headers["host"]}/#{t.id}"
-
-    # runs model validate and produces Clear::Model::InvalidError if not valid
-    t.save!
-
-    render text: t.to_json
+    Clear::SQL.with_savepoint do
+      t.url = "https://#{request.headers["host"]}/#{t.id}" # if t.id.created?
+      t.save!
+      Clear::SQL.rollback if !t.valid?
+      Clear::SQL.with_savepoint do
+        render text: t.to_json
+      end
+    end
   end
 
   delete "/" do
@@ -53,22 +38,22 @@ class Tasks < Application
   end
 
   def destroy
-    d = Task.query.select({id: params["id"]}).to_delete.execute
-
+    Task.query.select({id: params["id"]}).to_delete.execute
     render text: ""
   end
 
   def update
-    t : Task = Task.query.find({id: params["id"]}).as(Task)
-
+    t : Task = task(params["id"])
     h = JSON.parse(request.body.as(IO)).as_h
-
     t.title = h["title"].to_s if h.has_key?("title")
     t.order = h["order"].to_s.to_i if h.has_key?("order")
-
     t.completed = true if h.has_key?("completed")
-    t.save!
-
+    t.save! if t.valid?
     render text: t.to_json
+  end
+
+  # helpers
+  def task(id_in)
+    Task.query.find({id: id_in}).as(Task)
   end
 end
