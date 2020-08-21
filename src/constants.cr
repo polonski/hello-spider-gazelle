d837fee87e53f5f0bdd8454a9e4e2d94d9e6b77b
Michał Połoński <polonski@gmail.com>
2020-08-21 14:47:25 +1000

    chore(db_change):trying to play with heroku nicely

--- src/constants.cr
+++ src/constants.cr
@@ -13,13 +13,10 @@ module App
   ENV["PG_DATABASE_URL"] = "postgresql://postgres:postgres@localhost/todo_tasks"
 
   # settings for heroku
-  if ENV.has_key?("DATABASE_URL")
-    DEFAULT_PORT = 34464
-    DEFAULT_HOST = "0.0.0.0"
-  else
-    DEFAULT_PORT = (ENV["SG_SERVER_PORT"]? || 3000).to_i
-    DEFAULT_HOST = ENV["SG_SERVER_HOST"]? || "127.0.0.1"
-  end
+ 
+  DEFAULT_PORT = ENV.has_key?("DATABASE_URL") ? 34464 : (ENV["SG_SERVER_PORT"]? || 3000).to_i
+  DEFAULT_HOST = ENV.has_key?("DATABASE_URL") ? "0.0.0.0" : ENV["SG_SERVER_HOST"]? || "127.0.0.1"
+
 
   DEFAULT_PROCESS_COUNT = (ENV["SG_PROCESS_COUNT"]? || 1).to_i
 
