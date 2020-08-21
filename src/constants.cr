5d20581a76fa95add139dcb07d628dc6646d682f
Michał Połoński <polonski@gmail.com>
2020-08-21 15:31:14 +1000

    chore(db_change):trying to play with travis nicely

--- src/constants.cr
+++ src/constants.cr
@@ -13,10 +13,9 @@ module App
   ENV["PG_DATABASE_URL"] = "postgresql://postgres:postgres@localhost/todo_tasks"
 
   # settings for heroku
- 
+
   DEFAULT_PORT = ENV.has_key?("DATABASE_URL") ? 34464 : (ENV["SG_SERVER_PORT"]? || 3000).to_i
   DEFAULT_HOST = ENV.has_key?("DATABASE_URL") ? "0.0.0.0" : ENV["SG_SERVER_HOST"]? || "127.0.0.1"
-
 
   DEFAULT_PROCESS_COUNT = (ENV["SG_PROCESS_COUNT"]? || 1).to_i
 
